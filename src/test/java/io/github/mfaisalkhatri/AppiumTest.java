2cf6289e276d93ed000aa639f83178505db75597
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2023-03-25 08:02:22 +0530

    added tests to handle alert and notification in android app (#15)
    * added tests to handle alert and notification in android app
    
    * reformatting done

--- src/test/java/io/github/mfaisalkhatri/AppiumTest.java
+++ src/test/java/io/github/mfaisalkhatri/AppiumTest.java
@@ -1,13 +1,11 @@
 package io.github.mfaisalkhatri;
 
-import static org.testng.Assert.assertEquals;
-
-import io.github.mfaisalkhatri.pages.wdio.FormPage;
-import io.github.mfaisalkhatri.pages.wdio.HomePage;
-import io.github.mfaisalkhatri.pages.wdio.SignUpPage;
-import io.github.mfaisalkhatri.pages.wdio.SwipePage;
-import io.github.mfaisalkhatri.pages.wdio.WebViewPage;
+import io.github.mfaisalkhatri.pages.proverbial.Notifications;
+import io.github.mfaisalkhatri.pages.wdio.*;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /**
  * @author Faisal Khatri
@@ -16,65 +14,76 @@ import org.testng.annotations.Test;
 public class AppiumTest extends BaseTest {
 
     @Test
-    public void testHomePageTitle () {
-        HomePage homePage = new HomePage ();
-        assertEquals (homePage.getTitle (), "WEBDRIVER");
-        assertEquals (homePage.tagLine (), "Demo app for the appium-boilerplate");
+    public void testHomePageTitle() {
+        HomePage homePage = new HomePage();
+        assertEquals(homePage.getTitle(), "WEBDRIVER");
+        assertEquals(homePage.tagLine(), "Demo app for the appium-boilerplate");
     }
 
     @Test
-    public void testSignUp () {
-        SignUpPage signUpPage = new SignUpPage ();
-        signUpPage.signUp ("test@email.com", "Pass@12345");
-        assertEquals (signUpPage.getSuccessMessageTitle (), "Signed Up!");
-        assertEquals (signUpPage.getSuccessMessage (), "You successfully signed up!");
-        signUpPage.closeSuccessMessage ();
+    public void testSignUp() {
+        SignUpPage signUpPage = new SignUpPage();
+        signUpPage.signUp("test@email.com", "Pass@12345");
+        assertEquals(signUpPage.getSuccessMessageTitle(), "Signed Up!");
+        assertEquals(signUpPage.getSuccessMessage(), "You successfully signed up!");
+        signUpPage.closeSuccessMessage();
     }
 
     @Test
-    public void testForm () {
+    public void testForm() {
         final String inputText = "This is Appium Test";
-        FormPage formPage = new FormPage ();
-        formPage.fillForm (inputText, 2);
-        assertEquals (formPage.getInputText (), inputText);
-        assertEquals (formPage.getSwitchText (), "Click to turn the switch OFF");
-        assertEquals (formPage.getSelectedDropdownValue (), "Appium is awesome");
-        formPage.submitForm ();
+        FormPage formPage = new FormPage();
+        formPage.fillForm(inputText, 2);
+        assertEquals(formPage.getInputText(), inputText);
+        assertEquals(formPage.getSwitchText(), "Click to turn the switch OFF");
+        assertEquals(formPage.getSelectedDropdownValue(), "Appium is awesome");
+        formPage.submitForm();
 
-        assertEquals (formPage.getActiveMessageTitle (), "This button is");
-        assertEquals (formPage.getActiveMessage (), "This button is active");
-        formPage.closeMessage ();
+        assertEquals(formPage.getActiveMessageTitle(), "This button is");
+        assertEquals(formPage.getActiveMessage(), "This button is active");
+        formPage.closeMessage();
 
-        assertEquals (formPage.checkInActiveBtn (), "false");
-
-    }
-
-    @Test
-    public void testSwipeOnElement () {
-        SwipePage swipePage = new SwipePage ();
-        swipePage.performHorizontalSwipe ();
-        swipePage.performVerticalSwipe ();
+        assertEquals(formPage.checkInActiveBtn(), "false");
 
     }
 
     @Test
-    public void testSwipeTillElement () {
+    public void testSwipeOnElement() {
+        SwipePage swipePage = new SwipePage();
+        swipePage.performHorizontalSwipe();
+        swipePage.performVerticalSwipe();
 
-        SwipePage swipePage = new SwipePage ();
-        assertEquals (swipePage.swipeTillElement (), "You found me!!!");
     }
 
     @Test
-    public void testSwipeUsingScrollIntoView () {
-        SwipePage swipePage = new SwipePage ();
-        assertEquals (swipePage.swipeAndFindElement (), "You found me!!!");
+    public void testSwipeTillElement() {
+
+        SwipePage swipePage = new SwipePage();
+        assertEquals(swipePage.swipeTillElement(), "You found me!!!");
     }
 
     @Test
-    public void testWebView () {
-        WebViewPage webViewPage = new WebViewPage ();
+    public void testSwipeUsingScrollIntoView() {
+        SwipePage swipePage = new SwipePage();
+        assertEquals(swipePage.swipeAndFindElement(), "You found me!!!");
+    }
 
-        assertEquals (webViewPage.getMainPageText (),"Next-gen browser and mobile automation test framework for Node.js");
-        webViewPage.switchToNativeApp ();
+    @Test
+    public void testWebView() {
+        WebViewPage webViewPage = new WebViewPage();
+
+        assertEquals(webViewPage.getMainPageText(), "Next-gen browser and mobile automation test framework for Node.js");
+        webViewPage.switchToNativeApp();
+    }
+
+    @Test
+    public void testNotification() {
+        Notifications notifications = new Notifications();
+        assertTrue(notifications.checkNotificationIsDisplayed());
+        notifications.openNotificationPanel();
+
+        assertEquals(notifications.getAppNotificationTitle(), "Proverbial");
+        assertEquals(notifications.getNotificationTitle(), "Test Notification");
+        assertEquals(notifications.getNotificationText(), "Please enjoy this notification");
     }
 }
---
+++ src/test/java/io/github/mfaisalkhatri/pages/proverbial/Notifications.java
@@ -0,0 +1,46 @@
+package io.github.mfaisalkhatri.pages.proverbial;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.WebElement;
+
+import static io.github.mfaisalkhatri.drivers.AndroidDriverManager.getDriver;
+
+public class Notifications {
+
+
+    private WebElement expandNotificationBtn() {
+        return getDriver().findElements(AppiumBy.id("android:id/expand_button_touch_container")).get(0);
+    }
+
+    private WebElement notificationBtn() {
+        return getDriver().findElement(AppiumBy.id("com.lambdatest.proverbial:id/notification"));
+    }
+
+    private WebElement notificationBar() {
+        return getDriver().findElement(AppiumBy.id("com.lambdatest.proverbial:id/action_bar"));
+
+    }
+
+    public void openNotificationPanel() {
+        getDriver().openNotifications();
+    }
+
+    public boolean checkNotificationIsDisplayed() {
+        notificationBtn().click();
+        return notificationBar().isDisplayed();
+    }
+
+    public String getAppNotificationTitle() {
+        expandNotificationBtn().click();
+        return getDriver().findElements(AppiumBy.id("android:id/app_name_text")).get(0).getText();
+    }
+
+    public String getNotificationTitle() {
+        return getDriver().findElement(AppiumBy.id("android:id/title")).getText();
+    }
+
+    public String getNotificationText() {
+        return getDriver().findElement(AppiumBy.id("android:id/text")).getText();
+    }
+
+}
