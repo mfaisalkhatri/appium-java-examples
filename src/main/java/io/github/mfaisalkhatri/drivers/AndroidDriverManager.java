5144d513c2f9382ee991ecd46cb787d8c3fabcf9
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2023-07-08 14:38:17 +0530

    added firefox browser code example (#34)

--- src/main/java/io/github/mfaisalkhatri/drivers/AndroidDriverManager.java
+++ src/main/java/io/github/mfaisalkhatri/drivers/AndroidDriverManager.java
@@ -1,119 +1,134 @@
 package io.github.mfaisalkhatri.drivers;
 
+import static io.github.mfaisalkhatri.server.AppiumServerManager.startServer;
+import static io.github.mfaisalkhatri.server.AppiumServerManager.stopServer;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.HashMap;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.gecko.options.GeckoOptions;
 import io.appium.java_client.remote.AutomationName;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.nio.file.Path;
-import java.time.Duration;
-
-import static io.github.mfaisalkhatri.server.AppiumServerManager.*;
 
 /**
  * @author Faisal Khatri
  * @since 10/13/2022
  **/
 public class AndroidDriverManager {
-    private static final String APP_PATH = String.valueOf(
-            Path.of(System.getProperty("user.dir"), "/src/test/resources/app", "webdriverio-app.apk"));
-    private static final ThreadLocal<AndroidDriver> DRIVER = new ThreadLocal<>();
-    private static final Logger LOG = LogManager.getLogger("DriverManager.class");
+    private static final String                     APP_PATH = String.valueOf (
+        Path.of (System.getProperty ("user.dir"), "/src/test/resources/app", "webdriverio-app.apk"));
+    private static final ThreadLocal<AndroidDriver> DRIVER   = new ThreadLocal<> ();
+    private static final Logger                     LOG      = LogManager.getLogger ("DriverManager.class");
 
-    public static void quitSession() {
-        if (null != DRIVER.get()) {
-            LOG.info("Closing the driver...");
-            getDriver().quit();
-            DRIVER.remove();
-            stopServer();
+    public static void createAndroidDriver () {
+        startServer ("android");
+        //        setDriver (new AndroidDriver (getService ().getUrl (), geckoOptionsFirefox ()));
+        try {
+            setDriver (new AndroidDriver (new URL ("http://127.0.0.1:4723/wd/hub"), geckoOptionsFirefox ()));
+        } catch (final MalformedURLException e) {
+            throw new RuntimeException (e);
+        }
+        setupDriverTimeouts ();
+    }
+
+    public static AndroidDriver getDriver () {
+        return AndroidDriverManager.DRIVER.get ();
+    }
+
+    public static void quitSession () {
+        if (null != DRIVER.get ()) {
+            LOG.info ("Closing the driver...");
+            getDriver ().quit ();
+            DRIVER.remove ();
+            stopServer ();
         }
     }
 
-    public static AndroidDriver getDriver() {
-        return AndroidDriverManager.DRIVER.get();
+    private static GeckoOptions geckoOptionsFirefox () {
+        final HashMap<String, Object> firefoxOptions = new HashMap<> ();
+        firefoxOptions.put ("androidPackage", "org.mozilla.firefox");
+        //firefoxOptions.put ("androidDeviceSerial", "EMULATOR32X1X12X0");
+        final GeckoOptions geckoOptions;
+        geckoOptions = new GeckoOptions ().setPlatformName ("mac")
+            .setAutomationName (AutomationName.GECKO)
+            .setMozFirefoxOptions (firefoxOptions)
+            .setAndroidStorage ("app")
+            .setAcceptInsecureCerts (true)
+            .setNoReset (false);
+        return geckoOptions;
     }
 
-    private static void setDriver(AndroidDriver driver) {
-        AndroidDriverManager.DRIVER.set(driver);
+    private static void setDriver (final AndroidDriver driver) {
+        AndroidDriverManager.DRIVER.set (driver);
     }
 
-    private static UiAutomator2Options uiAutomator2OptionsWdio() {
-        UiAutomator2Options uiAutomator2Options;
-        uiAutomator2Options = new UiAutomator2Options()
-                .setAvd("Pixel_XL_API_33")
-                .setAvdLaunchTimeout(Duration.ofSeconds(300))
-                .setAvdReadyTimeout(Duration.ofSeconds(100))
-                .setDeviceName("Pixel_XL_API_33")
-                .setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2)
-                .setApp(APP_PATH)
-                .setAppPackage("com.wdiodemoapp")
-                .setAppActivity("com.wdiodemoapp.MainActivity")
-                .setNoReset(false);
+    private static void setupDriverTimeouts () {
+        getDriver ().manage ()
+            .timeouts ()
+            .implicitlyWait (Duration.ofSeconds (5));
+    }
+
+    //    private static DesiredCapabilities setCapabilities() {
+    //        DesiredCapabilities capabilities = new DesiredCapabilities();
+    //        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+    //        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_5_API_30");
+    //        capabilities.setCapability(MobileCapabilityType.APP, APP_PATH);
+    //        capabilities.setCapability("appPackage", "com.wdiodemoapp");
+    //        capabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+    //        capabilities.setCapability("noReset", false);
+    //        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
+    //        return capabilities;
+    //    }
+
+    private static UiAutomator2Options uiAutomator2OptionsChrome () {
+
+        final UiAutomator2Options uiAutomator2Options;
+        uiAutomator2Options = new UiAutomator2Options ().setAvd ("Pixel_6_API_31")
+            .setAvdLaunchTimeout (Duration.ofSeconds (300))
+            .setAvdReadyTimeout (Duration.ofSeconds (100))
+            .setDeviceName ("Pixel_6_API_31")
+            .setAutomationName (AutomationName.ANDROID_UIAUTOMATOR2)
+            .withBrowserName ("chrome")
+            .setAutoGrantPermissions (true)
+            .setNoReset (false);
+
         return uiAutomator2Options;
     }
 
-    private static UiAutomator2Options uiAutomator2OptionsProverbial() {
+    private static UiAutomator2Options uiAutomator2OptionsProverbial () {
 
-        UiAutomator2Options uiAutomator2Options;
-        uiAutomator2Options = new UiAutomator2Options().setAvd("Pixel_XL_API_33")
-                .setAvdLaunchTimeout(Duration.ofSeconds(300))
-                .setAvdReadyTimeout(Duration.ofSeconds(100))
-                .setDeviceName("Pixel_XL_API_33")
-                .setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2)
-                .setApp(APP_PATH)
-                .setAutoGrantPermissions(true)
-                .setAppPackage("com.lambdatest.proverbial")
-                .setAppActivity("com.lambdatest.proverbial.MainActivity")
-                .setNoReset(false);
+        final UiAutomator2Options uiAutomator2Options;
+        uiAutomator2Options = new UiAutomator2Options ().setAvd ("Pixel_XL_API_33")
+            .setAvdLaunchTimeout (Duration.ofSeconds (300))
+            .setAvdReadyTimeout (Duration.ofSeconds (100))
+            .setDeviceName ("Pixel_XL_API_33")
+            .setAutomationName (AutomationName.ANDROID_UIAUTOMATOR2)
+            .setApp (APP_PATH)
+            .setAutoGrantPermissions (true)
+            .setAppPackage ("com.lambdatest.proverbial")
+            .setAppActivity ("com.lambdatest.proverbial.MainActivity")
+            .setNoReset (false);
         return uiAutomator2Options;
     }
 
-    private static UiAutomator2Options uiAutomator2OptionsChrome() {
-
-        UiAutomator2Options uiAutomator2Options;
-        uiAutomator2Options = new UiAutomator2Options().setAvd("Pixel_6_API_31")
-                .setAvdLaunchTimeout(Duration.ofSeconds(300))
-                .setAvdReadyTimeout(Duration.ofSeconds(100))
-                .setDeviceName("Pixel_6_API_31")
-                .setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2)
-                .withBrowserName("chrome")
-                .setAutoGrantPermissions(true)
-                .setNoReset(false);
-
+    private static UiAutomator2Options uiAutomator2OptionsWdio () {
+        final UiAutomator2Options uiAutomator2Options;
+        uiAutomator2Options = new UiAutomator2Options ().setAvd ("Pixel_XL_API_33")
+            .setAvdLaunchTimeout (Duration.ofSeconds (300))
+            .setAvdReadyTimeout (Duration.ofSeconds (100))
+            .setDeviceName ("Pixel_XL_API_33")
+            .setAutomationName (AutomationName.ANDROID_UIAUTOMATOR2)
+            .setApp (APP_PATH)
+            .setAppPackage ("com.wdiodemoapp")
+            .setAppActivity ("com.wdiodemoapp.MainActivity")
+            .setNoReset (false);
         return uiAutomator2Options;
     }
-
-
-//    private static DesiredCapabilities setCapabilities() {
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_5_API_30");
-//        capabilities.setCapability(MobileCapabilityType.APP, APP_PATH);
-//        capabilities.setCapability("appPackage", "com.wdiodemoapp");
-//        capabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
-//        capabilities.setCapability("noReset", false);
-//        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
-//        return capabilities;
-//    }
-
-
-    public static void createAndroidDriver() {
-        startServer("android");
-        setDriver(new AndroidDriver(getService().getUrl(), uiAutomator2OptionsWdio()));
-//        try {
-//            setDriver(new AndroidDriver(new URL("http://localhost:4723/wd/hub"), uiAutomator2OptionsWdio()));
-//        } catch (MalformedURLException e) {
-//            throw new RuntimeException(e);
-//        }
-        setupDriverTimeouts();
-    }
-
-    private static void setupDriverTimeouts() {
-        getDriver().manage()
-                .timeouts()
-                .implicitlyWait(Duration.ofSeconds(5));
-    }
-
 
 }
--- src/test/java/io/github/mfaisalkhatri/android/tests/AndroidTests.java
+++ src/test/java/io/github/mfaisalkhatri/android/tests/AndroidTests.java
@@ -1,12 +1,17 @@
 package io.github.mfaisalkhatri.android.tests;
 
-import io.github.mfaisalkhatri.android.pages.browser.TheInternetPage;
-import io.github.mfaisalkhatri.android.pages.proverbial.Notifications;
-import io.github.mfaisalkhatri.android.pages.wdio.*;
-import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+
+import io.github.mfaisalkhatri.android.pages.browser.TheInternetPage;
+import io.github.mfaisalkhatri.android.pages.proverbial.Notifications;
+import io.github.mfaisalkhatri.android.pages.wdio.DragPage;
+import io.github.mfaisalkhatri.android.pages.wdio.FormPage;
+import io.github.mfaisalkhatri.android.pages.wdio.HomePage;
+import io.github.mfaisalkhatri.android.pages.wdio.SignUpPage;
+import io.github.mfaisalkhatri.android.pages.wdio.SwipePage;
+import io.github.mfaisalkhatri.android.pages.wdio.WebViewPage;
+import org.testng.annotations.Test;
 
 /**
  * @author Faisal Khatri
@@ -15,90 +20,99 @@ import static org.testng.Assert.assertTrue;
 public class AndroidTests extends BaseTest {
 
     @Test
-    public void testHomePageTitle() {
-        HomePage homePage = new HomePage();
-        assertEquals(homePage.getTitle(), "WEBDRIVER");
-        assertEquals(homePage.tagLine(), "Demo app for the appium-boilerplate");
+    public void testChrome () {
+        final TheInternetPage theInternetPage = new TheInternetPage ();
+        theInternetPage.navigateToInternetWebsite ();
+        assertEquals (theInternetPage.getPageHeader (), "Welcome to the-internet");
     }
 
     @Test
-    public void testSignUp() {
-        SignUpPage signUpPage = new SignUpPage();
-        signUpPage.signUp("test@email.com", "Pass@12345");
-        assertEquals(signUpPage.getSuccessMessageTitle(), "Signed Up!");
-        assertEquals(signUpPage.getSuccessMessage(), "You successfully signed up!");
-        signUpPage.closeSuccessMessage();
+    public void testDragAndDrop () {
+        final DragPage dragPage = new DragPage ();
+        dragPage.dragAndDropPrices ();
+        assertEquals (dragPage.congratulationsText (), "Congratulations");
     }
 
     @Test
-    public void testForm() {
+    public void testFirefoxBrowser () {
+        final TheInternetPage theInternetPage = new TheInternetPage ();
+        theInternetPage.navigateToInternetWebsite ();
+        assertEquals (theInternetPage.getPageHeader (), "Welcome to the-internet");
+    }
+
+    @Test
+    public void testForm () {
         final String inputText = "This is Appium Test";
-        FormPage formPage = new FormPage();
-        formPage.fillForm(inputText, 2);
-        assertEquals(formPage.getInputText(), inputText);
-        assertEquals(formPage.getSwitchText(), "Click to turn the switch OFF");
-        assertEquals(formPage.getSelectedDropdownValue(), "Appium is awesome");
-        formPage.submitForm();
+        final FormPage formPage = new FormPage ();
+        formPage.fillForm (inputText, 2);
+        assertEquals (formPage.getInputText (), inputText);
+        assertEquals (formPage.getSwitchText (), "Click to turn the switch OFF");
+        assertEquals (formPage.getSelectedDropdownValue (), "Appium is awesome");
+        formPage.submitForm ();
 
-        assertEquals(formPage.getActiveMessageTitle(), "This button is");
-        assertEquals(formPage.getActiveMessage(), "This button is active");
-        formPage.closeMessage();
+        assertEquals (formPage.getActiveMessageTitle (), "This button is");
+        assertEquals (formPage.getActiveMessage (), "This button is active");
+        formPage.closeMessage ();
 
-        assertEquals(formPage.checkInActiveBtn(), "false");
-
-    }
-
-    @Test
-    public void testSwipeOnElement() {
-        SwipePage swipePage = new SwipePage();
-        swipePage.performHorizontalSwipe();
-        swipePage.performVerticalSwipe();
+        assertEquals (formPage.checkInActiveBtn (), "false");
 
     }
 
     @Test
-    public void testSwipeTillElement() {
-
-        SwipePage swipePage = new SwipePage();
-        assertEquals(swipePage.swipeTillElement(), "You found me!!!");
+    public void testHomePageTitle () {
+        final HomePage homePage = new HomePage ();
+        assertEquals (homePage.getTitle (), "WEBDRIVER");
+        assertEquals (homePage.tagLine (), "Demo app for the appium-boilerplate");
     }
 
     @Test
-    public void testSwipeUsingScrollIntoView() {
-        SwipePage swipePage = new SwipePage();
-        assertEquals(swipePage.swipeAndFindElement(), "You found me!!!");
+    public void testNotification () {
+        final Notifications notifications = new Notifications ();
+        assertTrue (notifications.checkNotificationIsDisplayed ());
+        notifications.openNotificationPanel ();
+
+        assertEquals (notifications.getAppNotificationTitle (), "Proverbial");
+        assertEquals (notifications.getNotificationTitle (), "Test Notification");
+        assertEquals (notifications.getNotificationText (), "Please enjoy this notification");
     }
 
     @Test
-    public void testWebView() {
-        WebViewPage webViewPage = new WebViewPage();
-
-        assertEquals(webViewPage.getMainPageText(), "Next-gen browser and mobile automation test framework for Node.js");
-        webViewPage.switchToNativeApp();
+    public void testSignUp () {
+        final SignUpPage signUpPage = new SignUpPage ();
+        signUpPage.signUp ("test@email.com", "Pass@12345");
+        assertEquals (signUpPage.getSuccessMessageTitle (), "Signed Up!");
+        assertEquals (signUpPage.getSuccessMessage (), "You successfully signed up!");
+        signUpPage.closeSuccessMessage ();
     }
 
     @Test
-    public void testNotification() {
-        Notifications notifications = new Notifications();
-        assertTrue(notifications.checkNotificationIsDisplayed());
-        notifications.openNotificationPanel();
+    public void testSwipeOnElement () {
+        final SwipePage swipePage = new SwipePage ();
+        swipePage.performHorizontalSwipe ();
+        swipePage.performVerticalSwipe ();
 
-        assertEquals(notifications.getAppNotificationTitle(), "Proverbial");
-        assertEquals(notifications.getNotificationTitle(), "Test Notification");
-        assertEquals(notifications.getNotificationText(), "Please enjoy this notification");
     }
 
     @Test
-    public void testChrome() {
-        TheInternetPage theInternetPage = new TheInternetPage();
-        theInternetPage.navigateToInternetWebsite();
-        assertEquals(theInternetPage.getPageHeader(), "Welcome to the-internet");
+    public void testSwipeTillElement () {
+
+        final SwipePage swipePage = new SwipePage ();
+        assertEquals (swipePage.swipeTillElement (), "You found me!!!");
     }
 
     @Test
-    public void testDragAndDrop() {
-        DragPage dragPage = new DragPage();
-        dragPage.dragAndDropPrices();
-        assertEquals(dragPage.congratulationsText(), "Congratulations");
+    public void testSwipeUsingScrollIntoView () {
+        final SwipePage swipePage = new SwipePage ();
+        assertEquals (swipePage.swipeAndFindElement (), "You found me!!!");
     }
+
+    @Test
+    public void testWebView () {
+        final WebViewPage webViewPage = new WebViewPage ();
+
+        assertEquals (webViewPage.getMainPageText (),
+            "Next-gen browser and mobile automation test framework for Node.js");
+        webViewPage.switchToNativeApp ();
+    }
+
 }
